b5942cb2fa2a20a5ec10782def39d0b80bd1c911
Nathan Hunt <neighthan.hunt@gmail.com>
2020-01-01 14:48:05 -0500

    Added (mainly arxiv) metadata extraction.
    For any non-arxiv page, only the URL, time, and maybe the title (if there's an `<h1>` element) are extracted.

--- paper_helper/content.js
+++ paper_helper/content.js
@@ -1,6 +1,27 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.method == "extract_metadata")
-      sendResponse({title: "TEST"});
+    if (request.method == "extract_metadata") {
+      let metadata = {url: window.location.href, time: new Date().getTime()}
+      let host = window.location.host
+
+      if (host === "arxiv.org") {
+        let head = document.getElementsByTagName("head")[0]
+        let meta_tags = Array(...head.childNodes).filter(node => node.tagName === "META")
+        for (key of ["title", "date", "arxiv_id", "pdf_url"]) {
+          metadata[key] = meta_tags.filter(node => node.name === `citation_${key}`)[0].content
+        }
+        metadata["authors"] = meta_tags.filter(node => node.name === "citation_author").map(node => node.content)
+        metadata["abstract"] = meta_tags.filter(node => node.attributes[0].value === "og:description")[0].content
+      } else {
+        try {
+          metadata["title"] = document.getElementsByTagName("h1")[0].innerText
+        } catch (e) {
+          metadata["title"] = ""
+        }
+      }
+      sendResponse(metadata)
+    } else {
+      sendResponse({})
+    }
   }
 )
