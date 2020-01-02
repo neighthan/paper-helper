cad2502b1d8934b9ee4c3d3b30bdf07134e2ced7
Nathan Hunt <neighthan.hunt@gmail.com>
2020-01-02 15:27:26 -0500

    Add search bars to main page.

--- paper_helper/main.js
+++ paper_helper/main.js
@@ -3,7 +3,22 @@ let vue = new Vue({
   vuetify: new Vuetify({icons: {iconfont: "md"}}),
   data: {
     paper_data: [],
-    paper_data_loaded: false,
+    query: "",
+    query_tags: "",
+  },
+  computed: {
+    filtered_paper_data: function () {
+      let data = this.paper_data // direct reference; don't mutate!
+      if (this.query_tags != "") {
+        const query_tags = this.query_tags.toLowerCase().split(", ")
+        data = data.filter(pd => query_tags.every(tag => pd.search_tags.has(tag)))
+      }
+      if (this.query != "") {
+        const query = this.query.toLowerCase()
+        data = data.filter(pd => pd.search_string.includes(query))
+      }
+      return data
+    }
   },
 })
 
@@ -17,6 +32,7 @@ pd_promise.then(function(result) {
   console.log(paper_data)
   if (paper_data != null) {
     vue.paper_data = paper_data
+    paper_data.forEach(pd => pd.search_string = `${pd.title.toLowerCase()} ${pd.abstract.toLowerCase()}`)
+    paper_data.forEach(pd => pd.search_tags = new Set(pd.tags.map(tag => tag.toLowerCase())))
   }
-  vue.paper_data_loaded = true
 })
