3433e003ae9fd15f43af31cec2fa64501bb3b9b6
Nathan Hunt <neighthan.hunt@gmail.com>
2021-05-05 02:52:48 -0500

    Add basic page for writing notes.
    Currently it can't connect the notes to a given paper, so you can't save them, but you can type, and it will render in markdown in an adjacent pane.

---
+++ src/views/Notes.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="notes">
+    <v-app-bar app>
+      <NavIcon/>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <v-textarea v-model="text"></v-textarea>
+          </v-col>
+          <v-col v-html="markdown"></v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator"
+import NavIcon from "@/components/NavIcon.vue"
+import MarkdownIt from "markdown-it"
+import {DB} from "../db"
+
+const MdRenderer = new MarkdownIt()
+
+@Component({components: {NavIcon}})
+export default class Notes extends Vue {
+  text = ""
+
+  get markdown() {
+    return MdRenderer.render(this.text)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
