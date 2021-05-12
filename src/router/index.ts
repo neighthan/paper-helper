bae613c2671e1cb52051369ced655574e04bc407
Nathan Hunt <neighthan.hunt@gmail.com>
2021-05-12 00:25:29 -0500

    Rename Home.vue to Search.vue.
    In preparation for making the real home page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Search from "../views/Search.vue";
 import Settings from "../views/Settings.vue"
 import Notes from "../views/Notes.vue"
 
@@ -10,7 +10,7 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Search,
   },
   {
     path: "/settings",
