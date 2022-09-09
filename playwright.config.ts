66762908ccf1d63b7f7dfb3f987a8b29f496b761
Nathan Hunt <neighthan.hunt@gmail.com>
2022-09-09 12:17:25 -0400

    PW: save imgs + videos, add action timeout (5s).

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,12 +34,14 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
+    actionTimeout: 5000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
